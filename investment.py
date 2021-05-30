bacbb8933c34c24b689a691c10d46408e6654c21
uday <udaykhalsa@bitbucket.org>
2021-05-30 22:30:24 +0530

    Initial Commit

--- investment.py
+++ investment.py
@@ -10,21 +10,28 @@ user_investment_date_month = user_investment_date_input[3:5]
 user_investment_date_year = user_investment_date_input[6:]
 
 user_investment_date = user_investment_date.replace(year=int(user_investment_date_year), month=int(user_investment_date_month), day=int(user_investment_date_day))
+future_investment_date = user_investment_date +  datetime.timedelta(days=7)
+past_investment_date = user_investment_date -  datetime.timedelta(days=7)
 
 print(user_investment_date)
 
 
 def investment_calculator():
     day_difference = maturity_date - user_investment_date
-    day_difference = day_difference.days
+    past_day_difference = maturity_date - past_investment_date
+    future_day_difference = maturity_date - future_investment_date
+    
+    print(future_day_difference)
 
     interest_pd_percent = 15/365
 
     earning_pd = 850000*interest_pd_percent
 
-    investment_made = earning_pd *  day_difference                                                                                                                                                                                                                                               
+    investment_made = earning_pd * day_difference.days
+    future_investment_made = earning_pd * future_day_difference.days
+    past_investment_made = earning_pd * past_day_difference.days                                                                                                                                                                                                                             
 
-    return investment_made
+    return investment_made, future_investment_made, past_investment_made
 
 
 if user_investment_date > maturity_date:
@@ -32,4 +39,7 @@ if user_investment_date > maturity_date:
 else:
     investment = investment_calculator()
 
-print(investment)
+print(f'User invests {investment[0]} will give 850000 on {maturity_date}')
+print(f'User invests {investment[1]} 7 days from today will give 850000 on {maturity_date}')
+print(f'User invests {investment[2]} 7 days ago from today would give 850000 on {maturity_date}')
+
